22a6c826ff7fd083bfe77a2ae8a6be549efdd160
santoshtripathi1990 <er.santoshtripathi1990@gmail.com>
2015-11-07 10:25:21 -0800

    i have added one line

--- src/pkg1/TestGitRepo.java
+++ src/pkg1/TestGitRepo.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("I am first which will updated on the git hub");
 	System.out.println("I have added first change in this filr..");
 	System.out.println("I have added first change in this filr..");
+	System.out.println("i have added one more line");
 }
 }
